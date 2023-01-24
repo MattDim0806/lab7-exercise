7945cfbebccec3178d4323b683fafd0a0e33639d
MattDim0806 <yanghouling0806@gmail.com>
2023-01-24 13:57:00 +0800

    Initial Commit

---
+++ Lab7-exercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab7-exercise
+//
+//  Created by 楊皓麟 on 2023/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
